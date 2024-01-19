c6f85edfa914d9401592754136683a5cf6085299
Moonvel <48384066+Moonvel@users.noreply.github.com>
2024-01-19 10:04:05 +0300

    the distance function has been replaced by the distance method

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -11,9 +11,10 @@ public class MyFirstProgram {
         Rectangle r = new Rectangle(4,6);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-        Point p1 = new Point(3,8);
-        Point p2 = new Point(8,5);
-        System.out.println(Point.distance(p1,p2));
+        Point p = new Point(5,2,23,11);
+        System.out.println(p.distance());
+
+
     }
 
 }
