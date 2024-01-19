57cfab9b04514f8e5e061ced9354f4c5f133aab7
Moonvel <48384066+Moonvel@users.noreply.github.com>
2024-01-19 16:22:48 +0300

    GroupCreationTests refactoring

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -15,34 +15,55 @@ public class GroupCreationTests {
         System.setProperty("webdriver.chrome.driver", "C:\\Windows\\System32\\0409\\chromedriver.exe");
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
         wd.get("http://localhost/addressbook/group.php");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(username);
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
 
     @Test
     public void testGroupCreation() throws Exception {
-        wd.findElement(By.name("new")).click();
+        initGroupCreation();
+        fillGroupForm(new GroupData("123", "123", "123"));
+        submitGroupCreation();
+        returnToGroupPage();
+        getLogout();
+    }
+
+    private void getLogout() {
+        wd.findElement(By.linkText("Logout")).click();
+    }
+
+    private void returnToGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+
+    private void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    private void fillGroupForm(GroupData groupData) {
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys("123");
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
         wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys("123");
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
         wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys("123");
-        wd.findElement(By.name("submit")).click();
-        wd.findElement(By.linkText("home")).click();
-        wd.get("http://localhost/addressbook/");
-        wd.findElement(By.linkText("add new")).click();
-        wd.findElement(By.linkText("groups")).click();
-        wd.findElement(By.linkText("Logout")).click();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    private void initGroupCreation() {
+        wd.findElement(By.name("new")).click();
     }
 
     @AfterMethod(alwaysRun = true)
