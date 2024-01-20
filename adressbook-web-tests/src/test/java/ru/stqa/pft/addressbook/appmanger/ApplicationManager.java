bd7061079303dc637bcf6da164e96efb3d5fa55e
Moonvel <48384066+Moonvel@users.noreply.github.com>
2024-01-20 12:45:56 +0300

    Revert "error"
    This reverts commit f6c817a4c5d4c31326d131d776ef07b604cdd825.

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanger/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanger/ApplicationManager.java
@@ -3,42 +3,68 @@ package ru.stqa.pft.addressbook.appmanger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
     WebDriver wd;
 
-    private GroupHelper groupHelper;
-
     public void init() {
         System.setProperty("webdriver.chrome.driver", "C:\\Windows\\System32\\0409\\chromedriver.exe");
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         login("admin", "secret");
-        groupHelper = new GroupHelper(wd);
     }
 
     private void login(String username, String password) {
-        groupHelper.wd.get("http://localhost/addressbook/group.php");
-        groupHelper.wd.findElement(By.name("user")).click();
-        groupHelper.wd.findElement(By.name("user")).clear();
-        groupHelper.wd.findElement(By.name("user")).sendKeys(username);
-        groupHelper.wd.findElement(By.name("pass")).click();
-        groupHelper.wd.findElement(By.name("pass")).clear();
-        groupHelper.wd.findElement(By.name("pass")).sendKeys(password);
-        groupHelper.wd.findElement(By.xpath("//input[@value='Login']")).click();
+        wd.get("http://localhost/addressbook/group.php");
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
 
     public void getLogout() {
-        groupHelper.wd.findElement(By.linkText("Logout")).click();
+        wd.findElement(By.linkText("Logout")).click();
+    }
+
+    public void returnToGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+
+    public void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    public void fillGroupForm(GroupData groupData) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    public void initGroupCreation() {
+        wd.findElement(By.name("new")).click();
     }
 
     public void stop() {
-        groupHelper.wd.quit();
+        wd.quit();
     }
 
-    public GroupHelper getGroupHelper() {
-        return groupHelper;
+    public void deleteSelectedGroups() {
+      wd.findElement(By.name("delete")).click();
+    }
+
+    public void selectGroup() {
+      wd.findElement(By.name("selected[]")).click();
     }
 }
