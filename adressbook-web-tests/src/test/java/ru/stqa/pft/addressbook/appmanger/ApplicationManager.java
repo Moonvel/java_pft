af1e94a660522b9d13167e70d6556cf53760bedb
Moonvel <48384066+Moonvel@users.noreply.github.com>
2024-01-30 16:06:17 +0300

    new browser added; HelperBase class updated

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanger/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanger/ApplicationManager.java
@@ -3,21 +3,41 @@ package ru.stqa.pft.addressbook.appmanger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
-        WebDriver wd;
+    ;
+    WebDriver wd;
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private  NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private final String browser;
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Windows\\System32\\0409\\chromedriver.exe");
-        wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        if (browser.equals(BrowserType.CHROME)){
+            System.setProperty("webdriver.chrome.driver", "C:\\Windows\\System32\\0409\\chromedriver.exe");
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.FIREFOX)) {
+            System.setProperty("webdriver.gecko.driver", "C:\\Windows\\System32\\0409\\geckodriver.exe");
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.IE)){
+            System.setProperty("webdriver.ie.driver", "C:\\Windows\\System32\\0409\\IEDriverServer32.exe");
+            wd = new InternetExplorerDriver();
+
+        }
+
+
+        wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
