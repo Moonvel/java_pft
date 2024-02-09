792385e44f39667b8f186f6138e574fcad8d65a8
Moonvel <48384066+Moonvel@users.noreply.github.com>
2024-02-09 16:05:04 +0300

    element comparison has been replaced with list comparison in GroupTests
    method getGroupList implemented

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanger/GroupHelper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanger/GroupHelper.java
@@ -2,7 +2,11 @@ package ru.stqa.pft.addressbook.appmanger;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupHelper extends HelperBase {
 
@@ -52,10 +56,20 @@ public class GroupHelper extends HelperBase {
     }
 
     public boolean isThereAGroup() {
-        return isElementPresent(By.name("selected[]"));
+        return !isElementPresent(By.name("selected[]"));
     }
 
-    public int getGroupCount() {
-        return wd.findElements(By.name("selected[]")).size();
+  //  public int getGroupCount() {
+    //    return wd.findElements(By.name("selected[]")).size();}
+
+    public List<GroupData> getGroupList() {
+        List<GroupData> groups = new ArrayList<>();
+        List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+        for (WebElement element : elements){
+            String name = element.getText();
+            GroupData group = new GroupData(name, null, null);
+            groups.add(group);
+        }
+        return groups;
     }
 }
