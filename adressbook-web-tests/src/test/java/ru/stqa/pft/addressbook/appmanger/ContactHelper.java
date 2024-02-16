9f50f1c07b5366bca5647ef8fec552d7b5f53cad
Moonvel <48384066+Moonvel@users.noreply.github.com>
2024-02-16 17:40:18 +0300

    initContactModification changes in GroupHelper

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanger/ContactHelper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanger/ContactHelper.java
@@ -44,10 +44,7 @@ public class ContactHelper extends HelperBase {
     public void closeAlertWindow() {
         wd.switchTo().alert().accept();
     }
-    public void initContactModification(int index) {
-        wd.findElements(By.xpath("//img[@alt='Edit']")).get(index).click();
 
-    }
     public void submitContactModification() {
         click(By.name("update"));
     }
@@ -78,5 +75,20 @@ public class ContactHelper extends HelperBase {
         }
     return contacts;
     }
-
+    public void initContactModification(int id) {
+        WebElement checkbox = wd.findElement(By.cssSelector(String.format("input[value='%s']", id)));
+        WebElement row = checkbox.findElement(By.xpath("./../.."));
+        List<WebElement> cells = row.findElements(By.tagName("td"));
+        cells.get(7).findElement(By.tagName("a")).click();
+    }
+  /*  public ContactData infoFromEditForm(ContactData contact) {
+        initContactModification(0);
+        String firstName = wd.findElement(By.name("firstname")).getAttribute("value");
+        String lastname = wd.findElement(By.name("lastname")).getAttribute("value");
+        String home = wd.findElement(By.name("home")).getAttribute("value");
+        String mobile = wd.findElement(By.name("mobile")).getAttribute("value");
+        String work = wd.findElement(By.name("work")).getAttribute("value");
+        wd.navigate().back();
+        return null;
+    }*/
 }
--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -8,14 +8,14 @@ import java.util.Comparator;
 import java.util.List;
 
 public class ContactModificationTests extends TestBase {
-    @Test (enabled = false)
+    @Test (enabled = true)
     public void contactModificationTests(){
         app.goTo().gotoHomePage();
         if (!app.getContactHelper().isThereAContact()) {
             app.getContactHelper().createContact(new ContactData("Bob", "White", "Pause","+19123441212", "123"), true);
         }
         List<ContactData> before = app.getContactHelper().getContactList();
-        app.getContactHelper().initContactModification(before.size() - 1);
+        app.getContactHelper().initContactModification(before.get(before.size() - 1).getId());
         ContactData contact = new ContactData(before.get(before.size() - 1).getId(), "Lame", "Black", "Stop","+212123133", null);
         app.getContactHelper().fillContactForm(contact, false);
         app.getContactHelper().submitContactModification();
