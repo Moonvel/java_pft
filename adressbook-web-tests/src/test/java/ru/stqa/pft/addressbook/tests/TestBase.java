692bb8de374f8e785b6fee514f456c557498e6d0
Moonvel <48384066+Moonvel@users.noreply.github.com>
2024-02-21 23:41:15 +0300

    logger created

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,13 +1,17 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanger.ApplicationManager;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
-
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
@@ -15,11 +19,18 @@ public class TestBase {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.getLogout();
         app.stop();
-
+    }
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p){
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+    @AfterMethod (alwaysRun = true)
+    public void logTestStop(Method m){
+        logger.info("Stop test " + m.getName());
     }
 
 }
