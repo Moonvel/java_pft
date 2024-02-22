502386a403203b7bebc4342453a6e26a397ec68e
Moonvel <48384066+Moonvel@users.noreply.github.com>
2024-02-22 11:58:07 +0300

    ContactDataGenerator created

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,24 +1,44 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
-
-    @Test
-    public void testContactCreation(){
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/contacts.json"))){
+            StringBuilder json = new StringBuilder();
+            String line = reader.readLine();
+            while (line != null){
+                json.append(line);
+                line = reader.readLine();
+            }
+            Gson gson = new Gson();
+            List<ContactData> contacts = gson.fromJson(json.toString(), new TypeToken<List<ContactData>>(){}.getType());
+            return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+        }
+    }
+    @Test (dataProvider = "validContactsFromJson")
+    public void testContactCreation(ContactData contact){
         app.goTo().gotoHomePage();
         Contacts before = app.contact().all();
         File avatar = new File("src/test/resources/Avatar.jpg");
-        ContactData contact = new ContactData()
-                .withFirstName("Ivan").withLastName("White").withNickName("Pause").withAvatar(avatar);
-        app.contact().create(contact, true);
+        app.contact().create(contact.withAvatar(avatar), true);
         Contacts after = app.contact().all();
         assertThat(after.size(), equalTo(before.size() + 1));
         assertThat(after, equalTo(
