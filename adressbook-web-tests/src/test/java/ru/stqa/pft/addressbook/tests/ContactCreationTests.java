996c2239d6c0ced37b62ee8885a2af7c67e19886
Moonvel <48384066+Moonvel@users.noreply.github.com>
2024-02-27 10:02:41 +0300

    Comparison of data loaded from db in ContactCreationTests

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -36,10 +36,10 @@ public class ContactCreationTests extends TestBase {
     @Test (dataProvider = "validContactsFromJson")
     public void testContactCreation(ContactData contact){
         app.goTo().gotoHomePage();
-        Contacts before = app.contact().all();
+        Contacts before = app.db().contacts();
         File avatar = new File("src/test/resources/Avatar.jpg");
         app.contact().create(contact.withAvatar(avatar), true);
-        Contacts after = app.contact().all();
+        Contacts after = app.db().contacts();
         assertThat(after.size(), equalTo(before.size() + 1));
         assertThat(after, equalTo(
                 before.withAdded(contact.withId(after.stream().mapToInt(ContactData::getId).max().getAsInt()))));
