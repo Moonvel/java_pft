d2a9ec84aa35aa41421a2da3d33632efd0d6f497
Moonvel <48384066+Moonvel@users.noreply.github.com>
2024-02-27 15:05:43 +0300

    RegistrationTests class created. web driver initialization changed

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import javax.transaction.xa.XAResource;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
@@ -15,8 +16,9 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
 
     private final Properties properties;
-    WebDriver wd;
+    private WebDriver wd;
     private final String browser;
+    private RegistrationHelper registrationHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -26,28 +28,12 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
-
-
-        switch (browser) {
-            case BrowserType.CHROME:
-                System.setProperty("webdriver.chrome.driver", "C:\\Windows\\System32\\0409\\chromedriver.exe");
-                wd = new ChromeDriver();
-                break;
-            case BrowserType.FIREFOX:
-                System.setProperty("webdriver.gecko.driver", "C:\\Windows\\System32\\0409\\geckodriver.exe");
-                wd = new FirefoxDriver();
-                break;
-            case BrowserType.IE:
-                System.setProperty("webdriver.ie.driver", "C:\\Windows\\System32\\0409\\IEDriverServer32.exe");
-                wd = new InternetExplorerDriver();
-                break;
-        }
-        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get(properties.getProperty("web.baseUrl"));
     }
 
     public void stop() {
-        wd.quit();
+        if (wd != null){
+            wd.quit();
+        }
     }
 
     public HttpSession newSession() {
@@ -56,6 +42,35 @@ public class ApplicationManager {
 
     public String getProperty(String key) {
         return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper =  new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null){
+            switch (browser) {
+                case BrowserType.CHROME:
+                    System.setProperty("webdriver.chrome.driver", "C:\\Windows\\System32\\0409\\chromedriver.exe");
+                    wd = new ChromeDriver();
+                    break;
+                case BrowserType.FIREFOX:
+                    System.setProperty("webdriver.gecko.driver", "C:\\Windows\\System32\\0409\\geckodriver.exe");
+                    wd = new FirefoxDriver();
+                    break;
+                case BrowserType.IE:
+                    System.setProperty("webdriver.ie.driver", "C:\\Windows\\System32\\0409\\IEDriverServer32.exe");
+                    wd = new InternetExplorerDriver();
+                    break;
+            }
+            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
     }
 }
 
